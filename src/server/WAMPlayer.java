80ccdd6d977cd52e50e937408e793e9185a232f8
Cheng Ye <cy5914@rit.edu>
2019-04-07 13:46:46 -0400

    Worked on setting up WAMServer class, setting up initial server and client connections.

--- src/server/WAMPlayer.java
+++ src/server/WAMPlayer.java
@@ -1,4 +1,20 @@
 package server;
 
-public class WAMPlayer {
+import java.net.Socket;
+
+public class WAMPlayer implements Runnable {
+
+    public WAMPlayer( Socket player,
+                      int rows,
+                      int cols,
+                      int players,
+                      int playerNum ) {
+
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
